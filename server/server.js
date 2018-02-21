0a9e4aee884891cb52c8d238496147dc695ea7b3
novelistBaba <pus.novelist@gmail.com>
2018-02-21 19:16:03 +0530

    GET /todos/:id testing

--- server/server.js
+++ server/server.js
@@ -36,11 +36,11 @@ app.get('/todos/:id', ((req, res) => {
   if( !ObjectID.isValid(id)){
     return res.status(404).send();
   }
-  Todo.findById(id).then((doc) => {
-    if(!doc){
+  Todo.findById(id).then((todo) => {
+    if(!todo){
       return res.status(404).send();
     }
-      res.status(200).send({doc});
+      res.status(200).send({todo});
 
   }).catch((e) => {
     res.status(400).send();
