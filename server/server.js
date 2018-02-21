6f63e401996a5fd78df1cc2de041551f0932cc58
novelistBaba <pus.novelist@gmail.com>
2018-02-21 18:51:35 +0530

    GET /todos/:is method

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -29,6 +30,22 @@ app.get('/todos', (req,res) => {
   res.status(400).send(e);
 });
 });
+
+app.get('/todos/:id', ((req, res) => {
+  var id = req.params.id;
+  if( !ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((doc) => {
+    if(!doc){
+      return res.status(404).send();
+    }
+      res.status(200).send({doc});
+
+  }).catch((e) => {
+    res.status(400).send();
+  });
+}));
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
