ff944370912c3be76a5b8d178701acb82feb2879
novelistBaba <pus.novelist@gmail.com>
2018-02-21 20:10:01 +0530

    troubleshooting error in  heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,16 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+const options = {
+  useMongoClient: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', options);
 
 module.exports = {
   mongoose
